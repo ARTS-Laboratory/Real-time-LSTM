da7d9ef746a686475bfe77abd06e08f2d35a1164
Nile <dncoble@gmail.com>
2022-04-22 10:36:11 -0400

    push file changes

--- data/SNR.m
+++ data/SNR.m
@@ -6,64 +6,69 @@ close all;
 clear all;
 data = jsondecode(fileread('data_6_with_FFT.json'));
 % Coble Data     
-y_test = csvread("y_test.csv")';
-y_test = y_test(:,1);
-y_predict_2_5ms = csvread("\Users\dncob\Documents\GitHub\Real-time-LSTM\data\input_rate_outputs\2.5ms.csv")';
-y_predict_3ms = csvread("\Users\dncob\Documents\GitHub\Real-time-LSTM\data\input_rate_outputs\3ms.csv")';
-y_predict_3_5ms = csvread("\Users\dncob\Documents\GitHub\Real-time-LSTM\data\input_rate_outputs\3.5ms.csv")';
-y_predict_4ms = csvread("\Users\dncob\Documents\GitHub\Real-time-LSTM\data\input_rate_outputs\4ms.csv")';
-y_predict_4_5ms = csvread("\Users\dncob\Documents\GitHub\Real-time-LSTM\data\input_rate_outputs\4.5ms.csv")';
-y_predict_5ms = csvread("\Users\dncob\Documents\GitHub\Real-time-LSTM\data\input_rate_outputs\5ms.csv")';
-y_predict_5_5ms = csvread("\Users\dncob\Documents\GitHub\Real-time-LSTM\data\input_rate_outputs\5.5ms.csv")';
-y_predict_6ms = csvread("\Users\dncob\Documents\GitHub\Real-time-LSTM\data\input_rate_outputs\6ms.csv")';
-y_predict_6_5ms = csvread("\Users\dncob\Documents\GitHub\Real-time-LSTM\data\input_rate_outputs\6.5ms.csv")';
-y_predict_7ms = csvread("\Users\dncob\Documents\GitHub\Real-time-LSTM\data\input_rate_outputs\7ms.csv")';
-y_predict_7_5ms = csvread("\Users\dncob\Documents\GitHub\Real-time-LSTM\data\input_rate_outputs\7.5ms.csv")';
-y_predict_8ms = csvread("\Users\dncob\Documents\GitHub\Real-time-LSTM\data\input_rate_outputs\8ms.csv")';
-y_predict_8_5ms = csvread("\Users\dncob\Documents\GitHub\Real-time-LSTM\data\input_rate_outputs\8.5ms.csv")';
-y_predict_9ms = csvread("\Users\dncob\Documents\GitHub\Real-time-LSTM\data\input_rate_outputs\9ms.csv")';
-y_predict_9_5ms = csvread("\Users\dncob\Documents\GitHub\Real-time-LSTM\data\input_rate_outputs\9.5ms.csv")';
-y_predict_10ms = csvread("\Users\dncob\Documents\GitHub\Real-time-LSTM\data\input_rate_outputs\10ms.csv")';
+pred_data = csvread("forward pass prediction.csv");
+time = pred_data(:,1)';
+y_test = pred_data(:,2);
+y_predict = pred_data(:,3);
+% y_predict_2_5ms = csvread("\Users\dncob\Documents\GitHub\Real-time-LSTM\data\input_rate_outputs\2.5ms.csv")';
+% y_predict_3ms = csvread("\Users\dncob\Documents\GitHub\Real-time-LSTM\data\input_rate_outputs\3ms.csv")';
+% y_predict_3_5ms = csvread("\Users\dncob\Documents\GitHub\Real-time-LSTM\data\input_rate_outputs\3.5ms.csv")';
+% y_predict_4ms = csvread("\Users\dncob\Documents\GitHub\Real-time-LSTM\data\input_rate_outputs\4ms.csv")';
+% y_predict_4_5ms = csvread("\Users\dncob\Documents\GitHub\Real-time-LSTM\data\input_rate_outputs\4.5ms.csv")';
+% y_predict_5ms = csvread("\Users\dncob\Documents\GitHub\Real-time-LSTM\data\input_rate_outputs\5ms.csv")';
+% y_predict_5_5ms = csvread("\Users\dncob\Documents\GitHub\Real-time-LSTM\data\input_rate_outputs\5.5ms.csv")';
+% y_predict_6ms = csvread("\Users\dncob\Documents\GitHub\Real-time-LSTM\data\input_rate_outputs\6ms.csv")';
+% y_predict_6_5ms = csvread("\Users\dncob\Documents\GitHub\Real-time-LSTM\data\input_rate_outputs\6.5ms.csv")';
+% y_predict_7ms = csvread("\Users\dncob\Documents\GitHub\Real-time-LSTM\data\input_rate_outputs\7ms.csv")';
+% y_predict_7_5ms = csvread("\Users\dncob\Documents\GitHub\Real-time-LSTM\data\input_rate_outputs\7.5ms.csv")';
+% y_predict_8ms = csvread("\Users\dncob\Documents\GitHub\Real-time-LSTM\data\input_rate_outputs\8ms.csv")';
+% y_predict_8_5ms = csvread("\Users\dncob\Documents\GitHub\Real-time-LSTM\data\input_rate_outputs\8.5ms.csv")';
+% y_predict_9ms = csvread("\Users\dncob\Documents\GitHub\Real-time-LSTM\data\input_rate_outputs\9ms.csv")';
+% y_predict_9_5ms = csvread("\Users\dncob\Documents\GitHub\Real-time-LSTM\data\input_rate_outputs\9.5ms.csv")';
+% y_predict_10ms = csvread("\Users\dncob\Documents\GitHub\Real-time-LSTM\data\input_rate_outputs\10ms.csv")';
 
 %Raw Data
 x = data.acceleration_data';
-y = csvread("C:\Users\dncob\Documents\GitHub\Real-time-LSTM\data\y_test.csv")';
-y = y(1,:);
+y = y_test;
 signal = y;
-time = data.time_acceleration_data';
+% time = data.time_acceleration_data';
 
 % delete nan values from pin position
 nanidx=find(isnan(y));
-for i=nanidx
-    legitidx=find(~isnan(y(1:i)));
-    last_legitidx=legitidx(end);
-    y(i)=y(last_legitidx);
-end
+% for i=nanidx
+%     legitidx=find(~isnan(y(1:i)));
+%     last_legitidx=legitidx(end);
+%     y(i)=y(last_legitidx);
+% end
 
 %Resampling for zero order hold
-[L,M] = rat(numel(x)/numel(y));
-time = resample(time,M,L);
+% [L,M] = rat(numel(x)/numel(y));
+% time = resample(time,M,L);
 
-[L,M] = rat(numel(y_test)/numel(time));
-time_sub = resample(time,L,M);
+% [L,M] = rat(numel(y_test)/numel(time));
+% time_sub = resample(time,L,M);
 
 %Find SNR(dB)
-SNR_2_5ms = subsample_snr(signal, time, time_sub, y_predict_2_5ms)
-SNR_3ms = subsample_snr(signal, time, time_sub, y_predict_3ms)
-SNR_3_5ms = subsample_snr(signal, time, time_sub, y_predict_3_5ms)
-SNR_4ms = subsample_snr(signal, time, time_sub, y_predict_4ms)
-SNR_4_5ms = subsample_snr(signal, time, time_sub, y_predict_4_5ms)
-SNR_5ms = subsample_snr(signal, time, time_sub, y_predict_5ms)
-SNR_5_5ms = subsample_snr(signal, time, time_sub, y_predict_5_5ms)
-SNR_6ms = subsample_snr(signal, time, time_sub, y_predict_6ms)
-SNR_6_5ms = subsample_snr(signal, time, time_sub, y_predict_6_5ms)
-SNR_7ms = subsample_snr(signal, time, time_sub, y_predict_7ms)
-SNR_7_5ms = subsample_snr(signal, time, time_sub, y_predict_7_5ms)
-SNR_8ms = subsample_snr(signal, time, time_sub, y_predict_8ms)
-SNR_8_5ms = subsample_snr(signal, time, time_sub, y_predict_8_5ms)
-SNR_9ms = subsample_snr(signal, time, time_sub, y_predict_9ms)
-SNR_9_5ms = subsample_snr(signal, time, time_sub, y_predict_9_5ms)
-SNR_10ms = subsample_snr(signal, time, time_sub, y_predict_10ms)
+SNR_2_5ms = subsample_snr(signal, time, time, y_predict)
+hold on
+plot(time, signal)
+plot(time, y_predict)
+hold off
+% SNR_3ms = subsample_snr(signal, time, time_sub, y_predict_3ms)
+% SNR_3_5ms = subsample_snr(signal, time, time_sub, y_predict_3_5ms)
+% SNR_4ms = subsample_snr(signal, time, time_sub, y_predict_4ms)
+% SNR_4_5ms = subsample_snr(signal, time, time_sub, y_predict_4_5ms)
+% SNR_5ms = subsample_snr(signal, time, time_sub, y_predict_5ms)
+% SNR_5_5ms = subsample_snr(signal, time, time_sub, y_predict_5_5ms)
+% SNR_6ms = subsample_snr(signal, time, time_sub, y_predict_6ms)
+% SNR_6_5ms = subsample_snr(signal, time, time_sub, y_predict_6_5ms)
+% SNR_7ms = subsample_snr(signal, time, time_sub, y_predict_7ms)
+% SNR_7_5ms = subsample_snr(signal, time, time_sub, y_predict_7_5ms)
+% SNR_8ms = subsample_snr(signal, time, time_sub, y_predict_8ms)
+% SNR_8_5ms = subsample_snr(signal, time, time_sub, y_predict_8_5ms)
+% SNR_9ms = subsample_snr(signal, time, time_sub, y_predict_9ms)
+% SNR_9_5ms = subsample_snr(signal, time, time_sub, y_predict_9_5ms)
+% SNR_10ms = subsample_snr(signal, time, time_sub, y_predict_10ms)
 
 
 
@@ -73,6 +78,7 @@ function SNR = subsample_snr(signal, time, time_sub, y_pred)
     
     subsample_snr = [];
     signal_pred_zoh = myzoh(time,time_sub,double(y_pred));
+    length(signal_pred_zoh)
     % compute error signal from zero order hold
     error_signal = signal - signal_pred_zoh;
     % snip lead-in for MLP
@@ -116,7 +122,7 @@ end
 function signal = myzoh (x,x_sub,signal_in)
     
     signal = zeros(1,size(x,2));
-    
+    length(signal)
     i_sub = 1;
         
     for i=1:size(x,2)
