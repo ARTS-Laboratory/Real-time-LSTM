f1e04f10e55b3cbd465ee3668ee20d98731f0f25
Nile <dncoble@gmail.com>
2022-05-26 12:26:32 -0400

    upload sheet

---
+++ python training environment/load and save model.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+import tensorflow.keras as keras
+import numpy as np
+
+
+model = keras.models.load_model("./model_saves/400us3cells15units")
+# model = tf.saved_model.load("./model_saves/400us3cells15units")
+
+X = np.loadtxt("preprocessed_DROPBEAR_X.csv", delimiter=',')
+t = np.loadtxt("preprocessed_DROPBEAR_t.csv", delimiter=',')
+y = np.loadtxt("preprocessed_DROPBEAR_y.csv", delimiter=',')
+
+model.predict(X)
