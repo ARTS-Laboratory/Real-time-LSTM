2a8e18ed9c839d3592b7ffcc07f5c535806fa8ae
Nile <51173021+dncoble@users.noreply.github.com>
2022-10-19 18:34:54 -0400

    timing on os

---
+++ python training environment/model_timing_on_os.py
@@ -0,0 +1,89 @@
+import tensorflow.keras as keras
+from time import perf_counter
+from scipy import signal
+"""
+Testing the model timing on a GPOS (Windows)
+"""
+def preprocess(sampling_period):
+    import json
+    import pickle
+    import numpy as np
+    import sklearn as sk
+    import joblib
+    f = open('data_6_with_FFT.json')
+    data = json.load(f)
+    f.close()
+    
+    acc = np.array(data['acceleration_data'])
+    acc_t = np.array(data['time_acceleration_data'])
+    pin = np.array(data['measured_pin_location'])
+    pin_t = np.array(data['measured_pin_location_tt'])
+    
+    # pin contains some nan values
+    from math import isnan
+    for i in range(len(pin)):
+        if(isnan(pin[i])):
+            pin[i] = pin[i-1]
+    
+    resample_period = sampling_period
+    pin = pin[pin_t > 1.5]
+    pin_t = pin_t[pin_t > 1.5] - 1.5
+    acc = acc[acc_t > 1.5]
+    acc_t = acc_t[acc_t > 1.5] - 1.5
+    num = int((acc_t[-1] - acc_t[0])/resample_period)
+    
+    resample_acc, resample_t = signal.resample(acc, num, acc_t)
+    resample_pin = np.interp(resample_t, pin_t, pin)
+    
+    # scaling data, which means that it must be unscaled to be useful
+    from sklearn import preprocessing
+    acc_scaler = sk.preprocessing.StandardScaler()
+    acc_scaler.fit(resample_acc.reshape(-1, 1))
+    acc = acc_scaler.fit_transform(resample_acc.reshape(-1, 1)).flatten()
+    pin_scaler = sk.preprocessing.StandardScaler()
+    pin_scaler.fit(resample_pin.reshape(-1,1))
+    pin = pin_scaler.fit_transform(resample_pin.reshape(-1,1)).flatten().astype(np.float32)
+    
+    # reshape for multi-input
+    ds = 16
+    X = np.reshape(acc[:acc.size//ds*ds], (acc.size//ds, ds))
+    t = np.reshape(resample_t[:resample_t.size//ds*ds], (resample_t.size//ds, ds)).T[0]
+    y = np.reshape(pin[:pin.size//ds*ds], (pin.size//ds, ds)).T[0]
+    
+    X = np.expand_dims(X, 0)
+    
+    X_train = X[:,t<30.7]
+    y_train = y[t<30.7]
+    t_train = t[t<30.7]
+    
+    X_test = X[:,t>30.7]
+    y_test = y[t>30.7]
+    t_test = t[t>30.7]
+    
+    return (X, X_train, X_test), (y, y_train, y_test), (t, t_test, t_train), pin_scaler, acc_scaler
+#%% load model and resave weights
+model = keras.models.load_model('./model_saves/500us3cells15units')
+model.save_weights('./model_saves/500us3cells15unitsweights')
+#%%
+units = 15
+model = keras.Sequential([
+    keras.layers.LSTM(units,return_sequences=True,input_shape=[None, 16]),
+    keras.layers.LSTM(units,return_sequences=True),
+    keras.layers.LSTM(units,return_sequences=True),
+    keras.layers.TimeDistributed(keras.layers.Dense(1))
+])
+model.load_weights('./model_saves/500us3cells15unitsweights')
+#%%
+sample_period = 500*10**-6/16
+(X, X_train, X_test), (y, y_train, y_test), \
+    (t, t_test, t_train), pin_scaler, acc_scaler = preprocess(sample_period)
+
+print('beginning prediction...')
+start_time = perf_counter()
+y_pred = model.predict(X)
+stop_time = perf_counter()
+tot_time = stop_time - start_time
+avg_time = tot_time/X.shape[1]
+
+print('full pass prediction: %f s'%tot_time)
+print('per time step: %f us'%avg_time*10**6)
